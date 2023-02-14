feb2daa935c2050a060e5a4ba5f0e5c81253f284
LeandraLi <leandra2005@yandex.ru>
2023-02-14 20:19:30 +0300

    Добавлена задача 1 в семинаре 2 14.02.2023

---
+++ Seminars_C-Sharp/23-02-14_SEMINAR_2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу 
+// 78 - 8
+// 12 - 2
+// 85 - 2
+void MaxDecimal(int num)
+{
+    int ed = num % 10;
+    int dec = num / 10;
+    if(ed>dec)
+    {
+        Console.WriteLine($"Большая цифра числа {num} -> {ed}"); // return ed
+    }
+    else
+    {
+        Console.WriteLine($"Большая цифра числа {num} -> {dec}"); // return dec
+    }
+}
+int randomnumber = new Random().Next(10, 99 + 1);
+MaxDecimal(randomnumber);
