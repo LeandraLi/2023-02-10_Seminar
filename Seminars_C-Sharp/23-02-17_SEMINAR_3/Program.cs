46d102fc1a4eca50b5b86e3a2e03dbc22e7d9dc5
LeandraLi <leandra2005@yandex.ru>
2023-02-21 00:14:57 +0300

    Добавлены 2 задачи Семинара 3

--- Seminars_C-Sharp/23-02-17_SEMINAR_3/Program.cs
+++ Seminars_C-Sharp/23-02-17_SEMINAR_3/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1. Напишите программу, которая принимает на вход координаты точки x и y (причем, x и y не равны нулю), 
+// а выдаёт номер четверти плоскости, в которой находится эта точка.
+
+// int FindQuard(double x, double y)
+// {
+//     if(x>0 && y>0) return 1;
+//     if(x<0 && y>0) return 2;
+//     if(x<0 && y<0) return 3;
+//     if(x>0 && y<0) return 4;
+    
+//     return 0;
+// }
+
+// Console.WriteLine("Введите координату x: ");
+// int x = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координату y: ");
+// int y = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine($"Точка с указанными координатами находится в четверти: {FindQuard(x,y)}");
+
+
+// Задача 2. Напишите программу, которая принимает на вход координаты двух точек 
+// и находит расстояние между ними в 2D-пространстве.
+
+// double FindDistance(double xa, double ya, double xb, double yb)
+// {
+//     return Math.Round(Math.Sqrt(Math.Pow(xb-xa,2)+Math.Pow(yb-ya,2)),2);
+// }
+
+// Console.WriteLine("Введите координату x для первой точки: ");
+// double xa = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Введите координату y для первой точки: ");
+// double ya = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Введите координату x для второй точки: ");
+// double xb = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Введите координату y для второй точки: ");
+// double yb = Convert.ToDouble(Console.ReadLine());
+
+// Console.WriteLine($"Расстояние между нашими точками --> {FindDistance(xa, ya, xb, yb)}");
+
+
+// Задача 3. 
+
