2e9bdcaf959d2f8cbe38f858ec60431bd72b9ce3
LeandraLi <leandra2005@yandex.ru>
2023-02-20 22:36:54 +0300

    Добавлены задачи 2 и 3 по ДЗ-2_20.02.

--- Homeworks/WH_2023-02-14/Program.cs
+++ Homeworks/WH_2023-02-14/Program.cs
@@ -4,23 +4,50 @@
 // 782 -> 8
 // 918 -> 1
 
- int ShowDec (int num)
-{
-    int sot = num / 100;
-    int ed = num % 10;
-    return (num - (sot*100 + ed))/10;    
-}
-int randomnumber = new Random().Next(100, 1000);
-Console.WriteLine(randomnumber);
-ShowDec(randomnumber);
-Console.WriteLine(ShowDec(randomnumber));
+//  int ShowDec (int num)
+// {
+//     int sot = num / 100;
+//     int ed = num % 10;
+//     return (num - (sot*100 + ed))/10;    
+// }
+// int randomnumber = new Random().Next(100, 1000);
+// Console.WriteLine(randomnumber);
+// ShowDec(randomnumber);
+// Console.WriteLine(ShowDec(randomnumber));
 
 // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
- 
+// int ReadInt(string message)
+// {
+//     Console.Write(message);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
+// int MakeArray(int a, int b)
+// {
+// int result = 0;
+//     if (b < 3)
+//     {
+//         Console.Write("Третьей цифры нет, держи: ");
+//     }
+//     else
+//     {
+//         int c = 1;
+//         for (int i = b; i > 3; i--)
+//         {
+//             c = c * 10;
+//         }
+//         result = (a / c) % 10;
+//     }
+// return result;
+// }
+
+// int number = ReadInt("Введите число: ");
+// int count = number.ToString().Length;
+// Console.Write(MakeArray(number, count));
 
 
 // Задача 15: Напишите программу, которая принимает на вход цифру, 
@@ -30,4 +57,41 @@ Console.WriteLine(ShowDec(randomnumber));
 // 1 -> нет
 // Обычная проверка значения методом void - 
 
+// Console.Write("Введите число от 1 до 7: ");
+// int num = Convert.ToInt32(Console.ReadLine());
 
+// if (num >= 1 && num <= 7)
+//     {
+//         if (num == 6 | num == 7) Console.WriteLine("Сегодня выходной");
+//         else Console.WriteLine("Сегодня рабочий день");
+//     }
+// else Console.WriteLine("Ведено неверное число");
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+string WorkHoliday(int a)
+{
+    if (a > 0 && a < 8)
+    {
+        if (a == 7 || a == 6)
+        {
+            Console.Write("День под цифрой " + a + " - Выходной");
+        }
+        else
+        {
+            Console.Write("День под цифрой " + a + " - Рабочий");
+        }
+    }
+    else
+    {
+        Console.Write("Вы ввели число не в пределах от 1 до 7, поэтому невозможно определить");
+    }
+    return " день.";
+}
+
+int dayNumber = ReadInt("Введите число от 1 до 7: ");
+Console.WriteLine(WorkHoliday(dayNumber));
