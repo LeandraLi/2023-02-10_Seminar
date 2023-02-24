e4dba89ae67fbef50a03e010ac2e136ae2b1d0c3
LeandraLi <leandra2005@yandex.ru>
2023-02-24 21:27:54 +0300

    Исправлена последняя задача в группах с семинара 5

--- Seminars_C-Sharp/23-02-24_SEMINAR_5/Program.cs
+++ Seminars_C-Sharp/23-02-24_SEMINAR_5/Program.cs
@@ -177,55 +177,8 @@ Console.WriteLine("Input minimal value of array element");
 int minValue = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Input max value of array element");
 int maxValue = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Input num value");
-int num = Convert.ToInt32(Console.ReadLine());
 
 int[] nums = CreateRandomArray(size, minValue, maxValue);
 ShowArray(nums);
 Nums(nums);
 
-// вариант 2 последней задачи:
-
-// int[] CreateRandomArray(int size, int minValue, int maxValue)
-// {
-//     int[] array = new int[size];
-//     for (int i = 0; i < size; i++)
-//     {
-//         array[i] = new Random().Next(minValue, maxValue + 1);
-//     }
-//     return array;
-// }
-
-// void ShowArray(int[] array)
-// {
-//     for (int i = 0; i < array.Length; i++)
-//     {
-//         Console.Write(array[i] + " ");
-//     }
-//     Console.WriteLine();
-// }
-
-// void Nums(int[] array)
-// {
-//     int call = 0;
-//     for (int i = 0; i < array.Length; i++)
-//     {
-//         if ((array[i] >= 10) && (array[i] <= 99))
-//         {
-//             call++;
-//         }
-//     }
-//     Console.WriteLine($"В массиве значений которых лежат в отрезке [10,99] = {call}");
-// }
-
-// Console.WriteLine("Input array size");
-// int size = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Input minimal value of array element");
-// int minValue = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Input max value of array element");
-// int maxValue = Convert.ToInt32(Console.ReadLine());
-
-
-// int[] nums = CreateRandomArray(size, minValue, maxValue);
-// ShowArray(nums);
-// Nums(nums);
